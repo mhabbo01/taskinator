91506d3ed0d39d9a08bb42c8187fe98fda44336f
Matt Habbo <matthabbo45@gmail.com>
2022-02-21 17:56:36 -0500

    added HTML text through JavaScript for list items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,34 @@
 //we are giving the button with id #save-task the name of buttonE1 below.
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // addEventListener will listen for the click and will execute function
 var createTaskHandler = function() {
+    // prevents the default browser behavior
+    event.preventDefault();
+    //we are selecting an <input> element on the page that has a name attribute set to "task-name".
+    //adding .value on the end of this will grab the value of the input of the user
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    //console.dir will have the console display data as a JavaScript object, show virtually everything about the element.
+    // console.dir(taskNameInput);
     // it will create a new li element in the html
     var listItemEl = document.createElement("li");
-    // we will assign the new element the className to add the styles on the css sheet
     listItemEl.className = "task-item";
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // we will assign the new element the className to add the styles on the css sheet
+    taskInfoEl.className = "task-info";
     // this is the text that will display when we hit the button to add a new task
-    listItemEl.textContent = "This is a new task.";
+    //in here we are adding taskNameInput which will give us the value we retreived above
+    //add html content to div, must use innerHTML to insert this
+    listItemEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
     // appendChild add the new item to the end of the list.
+    listItemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(listItemEl);
 };
 
 // on a button click, create a task
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
